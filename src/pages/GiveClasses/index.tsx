faca94ab92253557b764c85f6c44d46a02e1c801
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-08 10:18:17 -0300

    corrigindo o tipo dos styles aquivos para typescript e criando style para giveClasses

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { View } from "react-native";
+import styles from "../Landing/styles";
 
 function GiveClasses() {
-  return <View></View>;
+  return <View style={styles.container}></View>;
 }
 
 export default GiveClasses;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Image, Text, TouchableOpacity } from "react-native";
+import { View, Image, Text } from "react-native";
 import styles from "./styles";
 import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
 import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
 
 function Landing() {
   const { navigate } = useNavigation();
@@ -24,18 +25,18 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon}></Image>
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity
+        <RectButton
           onPress={handleNavigateToGiveClassesPage}
           style={[styles.button, styles.buttonSecondary]}
         >
           <Image source={giveClassesIcon}></Image>
           <Text style={styles.buttonText}>Dar Aula</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
