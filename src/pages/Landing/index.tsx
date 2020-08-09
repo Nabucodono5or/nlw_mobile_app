8d843984f4b689c5dff4b5aa7544c8e1c2cf4ebe
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-09 09:33:20 -0300

    conectado connections com o servidor

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, Text } from "react-native";
-import styles from "./styles";
 import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
+import api from "../../services/api";
+import styles from "./styles";
 
 function Landing() {
   const { navigate } = useNavigation();
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      setTotalConnections(response.data.total);
+    });
+  }, []);
 
   function handleNavigateToGiveClassesPage() {
     navigate("GiveClasses");
@@ -47,7 +56,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões realizadas {"  "}
+        Total de {totalConnections} conexões realizadas {"  "}
         <Image source={heartIcon} />
       </Text>
     </View>
