cbf2cfd4ce115c9f16df7e6edf6afd25bacaba41
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-09 08:14:52 -0300

    adicionado cards para a favorites aba

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { View } from "react-native";
-import style from "./style";
+import { View, ScrollView } from "react-native";
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem/TeacherItem";
+import style from "./style";
 
 function Favorites() {
   return (
     <View style={style.container}>
       <PageHeader title="Meus Proffys favoritos" />
+      <ScrollView
+        style={style.teacherList}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
